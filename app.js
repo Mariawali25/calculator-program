bb8d212625dd4dfb01b15039ec8989a8244a9d04
mariawali25 <myaa26432@gmail.com>
2024-07-06 14:00:26 +0500

    initialize

---
+++ app.js
@@ -0,0 +1,23 @@
+//percentage calculator
+
+var alert =("welcome to my website");
+
+var totalmarks = (400);
+
+var engmarks = +prompt('enter your english marks');
+
+var urdumarks = +prompt('enter your urdu marks');
+
+var phymarks = +prompt('enter your physics marks');
+
+var chemmarks = +prompt('enter your chemistry marks');
+
+   var obtainedmarks = engmarks + urdumarks + phymarks + chemmarks;
+
+   var percentage = obtainedmarks/totalmarks * 100;
+
+   console.log("you got" + percentage + "%")
+
+   
+
+ 
